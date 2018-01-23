8a3f701a40d07552760ea26a096a3b7ae769bd78
Raymond Mao <raymond.mao@protonmail.com>
2018-01-23 15:53:24 +0100

    first commit

---
+++ src/config.rs
@@ -0,0 +1,31 @@
+use toml;
+use std::fs::File;
+use std::io::{Read, BufReader};
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub required: Required,
+    pub optional: Optional,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Required {
+    pub token: String,
+    pub prefix: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Optional {
+    pub setting: bool,
+}
+
+impl Config {
+    pub fn read_config(path: &str) -> Config {
+        let mut config = String::new();
+        let f = File::open(path).expect("Unable to open file");
+        let mut br = BufReader::new(f);
+        br.read_to_string(&mut config).expect("Unable to read string");
+        let config: Config = toml::from_str(&config).unwrap();
+        config
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serenity;
+extern crate toml;
+
+mod config;
+
+use config::Config;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, _: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say("Pong!") {
+                println!("Error sending message: {:?}", why);
+            }
+        } else if msg.content == "!messageme" {
+            if let Err(why) = msg.author.dm(|m| m.content("Hello!")) {
+                println!("Error when direct messaging user: {:?}", why);
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+fn main() {
+    let config = Config::read_config("./config/config.toml");
+    println!("{:?}", config);
+    /*assert_eq!(config.required.token, "ibimseintoken");
+    assert_eq!(config.optional.setting, true);*/
+    assert_eq!(config.required.prefix, "!");
+
+    let mut client = Client::new(&config.required.token, Handler).expect("Error creating client");
+
+    if let Err(why) = client.start() {
+        println!("Client error: {:?}", why);
+    }
+
+    /*if let Err(why) = client.start_shards(2) {
+        println!("Client error {:?}", why);
+    }*/
+}
