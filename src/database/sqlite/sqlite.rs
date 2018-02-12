635c16394a96c3ce27383971bfe15f84f08e0e20
BitSFitR <raymond.mao@protonmail.com>
2018-02-12 12:14:24 +0100

    use fern as a logger

--- src/database/sqlite/sqlite.rs
+++ src/database/sqlite/sqlite.rs
@@ -1,28 +1,36 @@
 use time;
 use time::Timespec;
+use chrono::DateTime;
 use std::fs;
 use std::path::Path;
 use rusqlite::Connection;
 use rusqlite::types::FromSql;
 
-#[derive(Debug)]
-struct Bot {
-    id: i64,
-    name: String,
-    time_created: Timespec,
-    chrono_timestamp: String,
-}
+//#[derive(Debug)]
+//struct Bot {
+//    id: i64,
+//    name: String,
+//    time_created: Timespec,
+//    chrono_timestamp: String,
+//}
 
 pub fn create_connection(&(ref path, ref loc): &(String, String)) -> Connection {
     if !Path::new(&loc).exists() {
         fs::create_dir_all(loc).expect("Error creating directory");
     }
-    let con = Connection::open(path).unwrap();
+    let con = match Connection::open(path) {
+        Ok(con) => con,
+        Err(why) => {
+            error!("Failed to create connection {}", why);
+            panic!()
+        }
+    };
     con
 }
 
 pub fn select_shard_uptime(con: Connection, shard: i64) -> Result<Vec<String>, ()> {
-    let mut stmt = con.prepare("SELECT chrono_timestamp FROM bot WHERE id = :id").unwrap();
+    let mut stmt = con.prepare("SELECT chrono_timestamp FROM bot WHERE id = :id")
+        .unwrap();
     let mut rows = stmt.query_named(&[(":id", &shard)]).unwrap();
     let mut stamp = Vec::new();
     while let Some(row) = rows.next() {
@@ -32,33 +40,31 @@ pub fn select_shard_uptime(con: Connection, shard: i64) -> Result<Vec<String>, (
     Ok(stamp)
 }
 
-pub fn create_bot_table(con: Connection) -> Connection {
-    con.execute(
-        "CREATE TABLE IF NOT EXISTS bot (id INTEGER PRIMARY KEY, name TEXT NOT NULL, time_created TEXT NOT NULL, chrono_timestamp TEXT NOT NULL)",
-        &[],
-    ).unwrap();
-    con
-}
-
-pub fn insert_timestamp(con: Connection, id: u64, name: String, timestamp: String) -> Connection {
-    let t = time::get_time();
-    con.execute(
-        "INSERT OR REPLACE INTO bot(id, name, time_created, chrono_timestamp) VALUES ($1, $2, $3, $4)",
-        &[&id.to_string(), &name, &t, &timestamp],
-    ).unwrap();
-    con
-}
-
-pub fn get_timestamp(con: Connection) {
-    let mut stmt = con.prepare("SELECT id, name, time_created, chrono_timestamp FROM bot")
-        .unwrap();
-    let bot_iter = stmt.query_map(&[], |row| Bot {
-        id: row.get(0),
-        name: row.get(1),
-        time_created: row.get(2),
-        chrono_timestamp: row.get(3),
-    }).unwrap();
-    for bot in bot_iter {
-        println!("Found bot {:?}", bot.unwrap());
+pub fn create_bot_table(con: &Connection) {
+    match con.execute("CREATE TABLE IF NOT EXISTS bot (id INTEGER PRIMARY KEY, name TEXT NOT NULL, time_created TEXT NOT NULL, chrono_timestamp TEXT NOT NULL)", &[]) {
+        Ok(_) => (),
+        Err(why) => error!("Error creating bot table: {}", why)
     }
 }
+
+pub fn insert_timestamp(con: &Connection, id: u64, name: String, timestamp: String) {
+    let t = time::get_time();
+    match con.execute("INSERT OR REPLACE INTO bot(id, name, time_created, chrono_timestamp) VALUES ($1, $2, $3, $4)", &[&id.to_string(), &name, &t, &timestamp]) {
+        Ok(i) => info!("Inserted {} row(s)", i),
+        Err(why) => error!("Error: {}", why)
+    }
+}
+
+//pub fn get_timestamp(con: Connection) {
+//    let mut stmt = con.prepare("SELECT id, name, time_created, chrono_timestamp FROM bot")
+//        .unwrap();
+//    let bot_iter = stmt.query_map(&[], |row| Bot {
+//        id: row.get(0),
+//        name: row.get(1),
+//        time_created: row.get(2),
+//        chrono_timestamp: row.get(3),
+//    }).unwrap();
+//    for bot in bot_iter {
+//        println!("Found bot {:?}", bot.unwrap());
+//    }
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate chrono;
+extern crate fern;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
@@ -20,14 +21,13 @@ use config::Config;
 use database::sqlite::sqlite;
 use commands::voice::VoiceManager;
 
+use std::{fs, thread};
 use std::sync::Arc;
 use std::collections::HashSet;
 use std::collections::HashMap;
-use std::thread;
-// use std::fs;
 // use std::fs::File;
 // use std::io::Write;
-// use std::path::Path;
+use std::path::Path;
 use std::time::Duration;
 use chrono::prelude::*;
 use serenity::prelude::*;
@@ -62,14 +62,14 @@ impl EventHandler for Handler {
             // Note that array index 0 is 0-indexed, while index 1 is 1-indexed.
             //
             // This may seem unintuitive, but it models Discord's behaviour.
-            println!(
+            info!(
                 "{} is connected on shard {}/{}!",
                 ready.user.name, shard[0], shard[1],
             );
             let con = sqlite::create_connection(&*SQLITE_PATH);
-            let con = sqlite::create_bot_table(con);
-            let con = sqlite::insert_timestamp(
-                con,
+            sqlite::create_bot_table(&con);
+            sqlite::insert_timestamp(
+                &con,
                 shard[0],
                 ready.user.name,
                 Utc::now().to_owned().to_string(),
@@ -98,10 +98,14 @@ impl Key for ShardManagerContainer {
 }
 
 fn main() {
+    match setup_logger() {
+        Ok(_) => (),
+        Err(why) => eprintln!("Failed to init logger: {}", why),
+    }
     //    let config = Config::get_config(config::CONFIG_PATH);
     //    println!("{:?}", config);
-    println!("Configuration file: {:?}", *CONFIG);
-    println!("SQLITE PATH: {:?}", *SQLITE_PATH);
+    debug!("Configuration file: {:?}", *CONFIG);
+    debug!("SQLITE PATH: {:?}", *SQLITE_PATH);
 
     let mut client = Client::new(&*CONFIG.required.token, Handler).expect("Error creating client");
 
@@ -121,7 +125,10 @@ fn main() {
 
             set
         }
-        Err(why) => panic!("Couldn't get application info: {:?}", why),
+        Err(why) => {
+            error!("Couldn't get application info: {}", why);
+            panic!()
+        }
     };
 
     client.with_framework(
@@ -133,8 +140,8 @@ fn main() {
                     .delimiters(vec![", ", ","])
             })
             .before(|ctx, _m, cmd_name| {
-                println!("{:?}", _m);
-                println!("Running command {}", cmd_name);
+                debug!("{:?}", _m);
+                debug!("Running command {}", cmd_name);
                 let mut data = ctx.data.lock();
                 let counter = data.get_mut::<CommandCounter>().unwrap();
                 let entry = counter.entry(cmd_name.to_string()).or_insert(0);
@@ -144,9 +151,9 @@ fn main() {
             .after(|_, _m, cmd_name, error| match error {
                 Ok(()) => {
                     let mut duration = Utc::now().signed_duration_since(_m.timestamp);
-                    println!("Command '{}' completed in {:#?}", cmd_name, duration);
+                    debug!("Command '{}' completed in {:#?}", cmd_name, duration);
                 }
-                Err(why) => println!("Command '{}' returned error {:?}", cmd_name, why),
+                Err(why) => error!("Command '{}' returned error {:?}", cmd_name, why),
             })
             .on_dispatch_error(|_ctx, msg, error| {
                 if let DispatchError::RateLimited(seconds) = error {
@@ -179,7 +186,7 @@ fn main() {
         let shard_runners = lock.runners.lock();
 
         for (id, runner) in shard_runners.iter() {
-            println!(
+            debug!(
                 "Shard ID {} is {} with a latency of {:?}",
                 id, runner.stage, runner.latency,
             );
@@ -188,8 +195,29 @@ fn main() {
 
     if let Err(why) = client
         .start_shards(CONFIG.required.shards)
-        .map_err(|why| println!("Client ended: {:?}", why))
-        {
-            println!("Client error: {:?}", why);
-        }
+        .map_err(|why| error!("Client ended: {:?}", why))
+    {
+        error!("Client error: {:?}", why);
+    }
+}
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    if !Path::new("./log").exists() {
+        fs::create_dir("./log").expect("Error creating folder")
+    };
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Utc::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("./log/output.log")?)
+        .apply()?;
+    Ok(())
 }
