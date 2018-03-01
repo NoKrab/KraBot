b2dadb463cdc67c1f9b838d061fa5ac8821edc5a
BitSFitR <raymond.mao@protonmail.com>
2018-03-01 09:50:32 +0100

    catch possibility of empty token and None

--- src/commands/voice.rs
+++ src/commands/voice.rs
@@ -333,11 +333,10 @@ command!(test(_ctx, msg, args) {
 //    let _ = msg.channel_id.say(query);
 });
 
-// Checks that a message successfully sent; if not, then logs why to stdout.
-// Is this even needed?
+/// Checks that a message successfully sent; if not, then logs why to stdout.
 fn check_msg(result: SerenityResult<Message>) {
     if let Err(why) = result {
-        error!("Error sending message: {:?}", why);
+        println!("Error sending message: {:?}", why);
     }
 }
 
@@ -351,63 +350,71 @@ fn string_to_static_str(s: String) -> &'static str {
 }
 
 fn youtube_search(query: String, msg: &Message) {
-    let token = match CONFIG.optional.youtube_token {
-        Some(ref token) => token.to_owned(),
-        None => panic!("no token"), // TODO allow "empty" token
-    };
-    let limit = 5;
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let client = Client::configure()
-        .connector(HttpsConnector::new(4, &handle).unwrap())
-        .build(&handle);
+    // Since Option is either None or Some, Some may just contain an empty String
+    if let Some(ref token) = CONFIG.optional.youtube_token {
+        let token = token.to_owned();
+        debug!("Youtbe API token: {}", token);
+        if !token.is_empty() {
+            let limit = 5;
+            let mut core = Core::new().unwrap();
+            let handle = core.handle();
+            let client = Client::configure()
+                .connector(HttpsConnector::new(4, &handle).unwrap())
+                .build(&handle);
 
-    let uri = format!(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&q={}&maxResults={}&key={}",
-        query, limit, token
-    );
-    let uri = &uri[..];
-    debug!("{}", uri);
+            let uri = format!(
+                "https://www.googleapis.com/youtube/v3/search?part=snippet&q={}&maxResults={}&key={}",
+                query, limit, token
+            );
+            let uri = &uri[..];
+            debug!("{}", uri);
 
-    let request = client
-        .request(Request::new(Method::Get, uri.parse().unwrap()))
-        .and_then(|res| {
-            debug!("GET: {}", res.status());
+            let request = client
+                .request(Request::new(Method::Get, uri.parse().unwrap()))
+                .and_then(|res| {
+                    debug!("GET: {}", res.status());
 
-            res.body().concat2().and_then(move |body| {
-                let v: Value = serde_json::from_slice(&body)
-                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-                Ok(v)
-            })
-        });
+                    res.body().concat2().and_then(move |body| {
+                        let v: Value = serde_json::from_slice(&body).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                        Ok(v)
+                    })
+                });
 
-    let result = core.run(request).unwrap();
+            let result = core.run(request).unwrap();
 
-    let items = match result["items"].as_array() {
-        Some(array) => array.to_owned(),
-        None => Vec::new(),
-    };
+            let items = match result["items"].as_array() {
+                Some(array) => array.to_owned(),
+                None => Vec::new(),
+            };
 
-    //    let items = result["items"].as_array();
-    //    debug!("{:#?}", result);
-    //    debug!("{}", result["etag"]);
-    //    debug!("{}", result["items"]);
-    debug!("{}", items.len());
-    debug!("{:#?}", items);
+            //    let items = result["items"].as_array();
+            //    debug!("{:#?}", result);
+            //    debug!("{}", result["etag"]);
+            //    debug!("{}", result["items"]);
+            debug!("{}", items.len());
+            debug!("{:#?}", items);
 
-    // m is a create_message struct not a Message struct!
-    // e is a create_embed struct
-    let _ = msg.channel_id.send_message(|m| m
-        .content("KONTENT")
-        .embed(|e| e
-            .author(|a| a
-                .name("KinG")
-                .icon_url("http://i0.kym-cdn.com/photos/images/newsfeed/001/339/830/135.png"))
-            .title("HALLO I BIMS EIN TITEL")
-            .thumbnail("http://i0.kym-cdn.com/photos/images/newsfeed/001/326/935/569.png")
-            .description("I BIMS EINE DESKRIPTION")
-            .colour(Colour::dark_teal())
-            .footer(|f| f
-                .icon_url("http://i0.kym-cdn.com/photos/images/newsfeed/001/331/733/acc.png")
-                .text("I BIMS DA FOOTER TECKST ECKS DEE"))));
+            // m is a create_message struct not a Message struct!
+            // e is a create_embed struct
+            let _ = msg.channel_id.send_message(|m| {
+                m.content("KONTENT").embed(|e| {
+                    e.author(|a| {
+                        a.name("KinG")
+                            .icon_url("http://i0.kym-cdn.com/photos/images/newsfeed/001/339/830/135.png")
+                    }).title("HALLO I BIMS EIN TITEL")
+                        .thumbnail("http://i0.kym-cdn.com/photos/images/newsfeed/001/326/935/569.png")
+                        .description("I BIMS EINE DESKRIPTION")
+                        .colour(Colour::dark_teal())
+                        .footer(|f| {
+                            f.icon_url("http://i0.kym-cdn.com/photos/images/newsfeed/001/331/733/acc.png")
+                                .text("I BIMS DA FOOTER TECKST ECKS DEE")
+                        })
+                })
+            });
+        } else {
+            let _ = msg.channel_id.say("Missing Youtube token!");
+        }
+    } else {
+        let _ = msg.channel_id.say("Missing Youtube token!");
+    }
 }
