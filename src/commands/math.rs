940d56f3d72819d05644e3c31104be667b65b80c
Habitbreaker <habitbreaker@live.de>
2018-04-07 17:33:39 +0200

    Removed commented code

--- src/commands/math.rs
+++ src/commands/math.rs
@@ -1,5 +1,3 @@
-//use std::time::SystemTime;
-
 command!(multiply(_ctx, msg, args) {
     let one = args.single::<f64>().unwrap();
     let two = args.single::<f64>().unwrap();
@@ -8,27 +6,6 @@ command!(multiply(_ctx, msg, args) {
 
     let _ = msg.channel_id.say(product);
 });
-
-//command!(fibonacci(_ctx, msg, args) {
-//    let n = args.single::<u64>().unwrap();
-//    let now = SystemTime::now();
-//    let fib = fib(n);
-//    let after = SystemTime::now();
-//    let difference = after.duration_since(now)
-//                            .expect("SystemTime::duration_since failed");
-//    let msg_content = format!("F({}) = {}; {:?}", n, fib, difference);
-//    let _ = msg.channel_id.say(msg_content);
-//});
-//
-//fn fib(n: u64) -> u64 {
-//    if n == 1 {
-//        return 1;
-//    } else if n == 0 {
-//        return 0;
-//    } else {
-//        fib(n - 1) + fib(n - 2)
-//    }
-//}
 
 command!(fibonacci(_ctx, msg, args) {
     let n = args.single::<u64>().unwrap();
