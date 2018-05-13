6983a6ae7fa4c3d91ed89142745c19f0db663dfd
Habitbreaker <habitbreaker@live.de>
2018-05-13 19:37:13 +0200

    Implemented !img command and changed message building back to 0.5.3 method

--- src/commands/imgur.rs
+++ src/commands/imgur.rs
@@ -1,6 +1,15 @@
 use serde_json::Value;
-use util::api::imgur::imgur::{Account};
+use util::api::imgur::imgur::*;
+use serenity::client::CACHE;
+use serenity::Result as SerenityResult;
+use serenity::model::channel::{Channel, Message};
+use regex::Regex;
+use rand;
+use rand::Rng;
 
+lazy_static! {
+    static ref RE_TAGS: Regex = Regex::new(r"(#[a-zA-Z]+\b)").unwrap();
+}
 
 command!(get_imgs(_ctx, msg, args) {
     if let Some(result) = Account::account_images() {
@@ -8,18 +17,19 @@ command!(get_imgs(_ctx, msg, args) {
                 Some(array) => array.to_owned(),
                 None => Vec::new(),
         };
-        let _ = msg.channel_id.send_message(|mut m| {
-            m.content("Imgur Images");
-            m.embed(|mut e| {
-            for img in &items {
-                e.field(&img["name"].as_str().unwrap(), &img["link"].as_str().unwrap(), false);
-            }
-            e
-            });
-            m
-        });
+        debug!("Imgs: {:?}", items);
+        check_msg(msg.channel_id.send_message(|m| {           
+            m.embed(|e| {
+                let mut e = e;
+                e = e.title("Imgur Images");
+                for img in &items {
+                    e = e.field(&img["name"].as_str().unwrap(), format!("Link: {}\nId: {}",&img["link"].as_str().unwrap(), &img["id"].as_str().unwrap()), false);
+                }
+                e
+            })
+        }));
     } else {
-        let _ = msg.channel_id.say("?????");
+        check_msg(msg.channel_id.say("Failed to retrieve images"));
     }
 });
 
@@ -29,17 +39,123 @@ command!(get_albums(_ctx, msg, args) {
                 Some(array) => array.to_owned(),
                 None => Vec::new(),
         };
-        let _ = msg.channel_id.send_message(|mut m| {
-            m.content("Imgur Albums");
-            m.embed(|mut e| {
-            for album in &albums {
-                e.field(&album["title"].as_str().unwrap(), &album["link"].as_str().unwrap(), false);
-            }
-            e
-            });
-            m
-        });
+        check_msg(msg.channel_id.send_message(|m| {
+            m.embed(|e| {
+                let mut e = e;
+                    e = e.title("Imgur Albums");
+                    for album in &albums {
+                        e = e.field(&album["title"].as_str().unwrap(), format!("Link: {}\nId: {}",&album["link"].as_str().unwrap(), &album["id"].as_str().unwrap()), false);
+                    }
+                    e
+                })
+        }));
     } else {
-        let _ = msg.channel_id.say("?????");
+        check_msg(msg.channel_id.say("Failed to retrieve albums"));
     }
 });
+
+command!(get_current_album(_ctx, msg) {
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Error finding channel info"));
+            return Ok(());
+        },
+    };
+    check_msg(msg.channel_id.say(get_current_album_id(guild_id.to_string().parse::<u64>().unwrap() as i64)));
+});
+
+command!(set_album(_ctx, msg, args) {
+    debug!("Channel ID: {}", msg.channel_id);
+    debug!("{:?}", msg.guild_id());
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Error finding channel info"));
+            return Ok(());
+        },
+    };
+    let album_id = match args.single::<String>() {
+        Ok(album_id) => album_id,
+        Err(_) => {
+            check_msg(msg.reply("Requires a valid album ID"));
+            return Ok(());
+        },
+    };
+    set_album_id(&album_id, guild_id.to_string().parse::<u64>().unwrap() as i64);
+});
+
+command!(query_img(_ctx, msg, args) {
+    //get album imgs
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Error finding channel info"));
+            return Ok(());
+        },
+    };
+
+     if let Some(result) = Album::album_images(guild_id.to_string().parse::<u64>().unwrap() as i64) {
+        let images = match result.as_array() {
+                Some(array) => array.to_owned(),
+                None => Vec::new(),
+        };
+        let mut best_match_count: i32 = -1;
+        let mut matched_imgs: Vec<usize> = Vec::new();
+        let args: Vec<&str> = args.full().split(' ').collect(); //Splits args of msg e.g. "a b" into ["a", "b"]
+
+        info!("{:?}", &args);
+        for (i, img) in images.iter().enumerate() {
+            let description = &img["description"].as_str().unwrap();
+            info!("Desc: {}", description);
+            let mut match_count: i32 = -1;
+            for mat in RE_TAGS.find_iter(description) {
+                info!("Tags Loop...");
+                for arg in &args {
+                    info!("Args Loop...[{}]", &arg);
+                    let tag: String = mat.as_str().to_owned().replace("#", "");
+                    info!("Tag: {}", &tag);
+                    if &tag == arg {
+                        info!("GEIL!!!");
+                        match_count = match_count + 1;
+                    } else {
+                        info!("Schade :(");
+                    }
+                }
+            }
+            info!("Matched: {}", match_count + 1);
+            if match_count > best_match_count {
+                matched_imgs.clear();
+                matched_imgs.push(i);
+                best_match_count = match_count;
+            } else if match_count > -1 && match_count == best_match_count {
+                matched_imgs.push(i);
+            }
+        }
+        info!("Result: {:?}", &matched_imgs);
+        info!("Result Length: {}", matched_imgs.len());
+        info!("Best match count: {}", best_match_count);
+        if best_match_count > -1 {
+            match matched_imgs.len() {
+                1 => check_msg(msg.channel_id.say(&images[matched_imgs[0]]["link"])),
+                _ => {
+                    let mut rng = rand::thread_rng();
+                    let number = rng.gen_range(0, matched_imgs.len());
+                    info!("RNG: {}", number);
+                    check_msg(msg.channel_id.say(&images[matched_imgs[number]]["link"]))
+                }
+            }
+        } else {
+            check_msg(msg.channel_id.say("No matching image found :("));
+        }
+    } else {
+        check_msg(msg.channel_id.say("Failed to retrieve albums"));
+    }
+});
+
+/// Checks that a message successfully sent; if not, then logs why to stdout.
+fn check_msg(result: SerenityResult<Message>) {
+    if let Err(why) = result {
+        println!("Error sending message: {:?}", why);
+    }
+}
