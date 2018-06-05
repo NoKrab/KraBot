4e49fd92a8c735d9f45972d1f7d86abb621cd3dc
Habitbreaker <habitbreaker@live.de>
2018-06-05 19:09:45 +0200

    Added audio structs

---
+++ src/audio/track_manager.rs
@@ -0,0 +1,37 @@
+use serenity::voice::{Handler, LockedAudio, AudioSource};
+use serenity::prelude::Mutex;
+use serenity::voice;
+
+pub struct AudioTrack {
+    source: Box<AudioSource>,
+    title: String
+}
+
+impl AudioTrack {
+    pub fn new(source: Box<AudioSource>, title: String) -> AudioTrack {
+        AudioTrack {
+            source,
+            title
+        }
+    }
+}
+
+pub struct TrackManager {
+    queue: Vec<AudioTrack>,
+    current: usize,
+}
+
+impl TrackManager {
+    pub fn new() -> TrackManager {
+        TrackManager {
+            queue: Vec::new(),
+            current: 0
+        }
+    }
+    pub fn add_track(mut self, track: AudioTrack) {
+        self.queue.push(track);
+    }
+    pub fn clear_queue(mut self) {
+        self.queue.clear();
+    }
+}
