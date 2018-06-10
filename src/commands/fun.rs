dbe8d73e594940e2fd04906a0483aed6c2bea37e
Habitbreaker <habitbreaker@live.de>
2018-06-10 02:43:48 +0200

    i made this

--- src/commands/fun.rs
+++ src/commands/fun.rs
@@ -1,22 +1,28 @@
 use util::api::twitch::twitch::API;
 use serenity::Result as SerenityResult;
 use serenity::model::channel::Message;
-
+use rand::Rng;
+use rand;
 
 command!(twitch(_ctx, msg, args) {
     let code = args.single::<String>().unwrap();
-    if let Some(emote_uri) = API::get_emote_uri(&code) {
+    if let Some(emote_uri) = API::get_emote_uri(&code) { 
         check_msg(msg.channel_id.say(&emote_uri));
     } else {
         check_msg(msg.channel_id.say("Emote not found!"));
     }
 });
 
-command!(flip(_ctx, _msg, _args) {
-
+command!(flip(_ctx, msg, _args) {
+    let mut rng = rand::thread_rng();
+    let paths = match  rng.gen_range(0, 2) {
+        0 => vec!["assets/coin_head.png"],
+        1 => vec!["assets/coin_tail.png"],
+        _ => vec![]
+    };
+    
+    check_msg( msg.channel_id.send_files(paths, |m| m.content("Rolling...")));
 });
-
-///------------------------------------Functions------------------------------------
 
 /// Checks that a message successfully sent; if not, then logs why to stdout.
 fn check_msg(result: SerenityResult<Message>) {
