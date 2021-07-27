cb2ada8daa9104accfa6ac0f97f7bc2bdda2e243
Daniel H <habitbreaer@live.de>
2021-07-27 18:21:12 +0200

    Add track selection

--- lib/src/discord/mod.rs
+++ lib/src/discord/mod.rs
@@ -14,7 +14,12 @@ use serenity::{
         StandardFramework,
     },
     http::Http,
-    model::{channel::Message, gateway::Ready, id::GuildId, misc::Mentionable},
+    model::{
+        channel::{Message, ReactionType},
+        gateway::Ready,
+        id::{EmojiId, GuildId},
+        misc::Mentionable,
+    },
     Result as SerenityResult,
 };
 
@@ -234,8 +239,9 @@ async fn play(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
         let lava_client = data.get::<Lavalink>().unwrap().clone();
 
         let query_information = lava_client.auto_search_tracks(&query).await?;
+        let tracks = query_information.tracks;
 
-        if query_information.tracks.is_empty() {
+        if tracks.is_empty() {
             check_msg(
                 msg.channel_id
                     .say(&ctx, "Could not find any video of the search query.")
@@ -244,9 +250,77 @@ async fn play(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
             return Ok(());
         }
 
+        let mut track_idx = 0;
+        let emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
+        let reactions = emojis
+            .iter()
+            .map(|emoji| emoji.parse().unwrap())
+            .collect::<Vec<ReactionType>>();
+
+        if tracks.len() > 1 {
+            if let Ok(react_msg) = msg
+                .channel_id
+                .send_message(&ctx.http, |m| {
+                    m.embed(|e| {
+                        e.title("🎵 Tracks found 🎵");
+                        for (idx, track) in tracks.iter().take(5).enumerate() {
+                            if let Some(ref info) = track.info {
+                                e.field(
+                                    format!("{} {}", emojis[idx], info.title),
+                                    &info.uri,
+                                    false,
+                                );
+                            } else {
+                                e.field(emojis[idx], "No track info", false);
+                            }
+                        }
+
+                        e
+                    });
+                    m.reactions(
+                        reactions
+                            .into_iter()
+                            .take(tracks.len())
+                            .collect::<Vec<ReactionType>>(),
+                    );
+
+                    m
+                })
+                .await
+            {
+                if let Some(reaction) = &react_msg
+                    .await_reaction(&ctx)
+                    .timeout(Duration::from_secs(10))
+                    .author_id(msg.author.id)
+                    .await
+                {
+                    let emoji = &reaction.as_inner_ref().emoji;
+                    match emoji.as_data().as_str() {
+                        "1️⃣" => track_idx = 0,
+                        "2️⃣" => track_idx = 1,
+                        "3️⃣" => track_idx = 2,
+                        "4️⃣" => track_idx = 3,
+                        "5️⃣" => track_idx = 4,
+                        _ => {
+                            check_msg(
+                                msg.channel_id
+                                    .say(&ctx.http, "Wrong reaction. You had one job.")
+                                    .await,
+                            );
+
+                            return Ok(());
+                        }
+                    }
+                } else {
+                    check_msg(msg.channel_id.say(&ctx.http, "Nothing selected").await);
+
+                    return Ok(());
+                }
+            }
+        }
+
         if let Err(why) = &lava_client
-            .play(guild_id, query_information.tracks[0].clone())
-            // Change this to play() if you want your own custom queue or no queue at all.
+            .play(guild_id, tracks[track_idx].clone())
             .queue()
             .await
         {
@@ -259,7 +333,7 @@ async fn play(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
                     &ctx.http,
                     format!(
                         "Added to queue: {}",
-                        query_information.tracks[0].info.as_ref().unwrap().title
+                        tracks[track_idx].info.as_ref().unwrap().title
                     ),
                 )
                 .await,
