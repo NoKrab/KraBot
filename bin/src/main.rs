886aad0d1c9df994a27d4604dd9ff7caf563e0ad
BitSFitR <raymond.mao@protonmail.com>
2021-08-06 01:06:50 +0200

    add version metadata as debug info

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -16,6 +16,13 @@ async fn main() {
         }
     }
     log4rs::init_file("./config/log4rs.toml", Default::default()).unwrap();
+    debug!("cargo bin version: {}", env!("CARGO_PKG_VERSION"));
+    debug!("build Timestamp: {}", env!("VERGEN_BUILD_TIMESTAMP"));
+    debug!(
+        "git branch: {}-{}",
+        env!("VERGEN_GIT_BRANCH"),
+        env!("VERGEN_GIT_SHA")
+    );
     print_2b();
     dotenv().ok();
     if let Err(e) = start().await {
