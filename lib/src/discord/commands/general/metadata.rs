9c8a662ec0493bfa73849dc096360586a6146aff
BitSFitR <raymond.mao@protonmail.com>
2021-08-22 03:53:49 +0200

    add embed

--- lib/src/discord/commands/general/metadata.rs
+++ lib/src/discord/commands/general/metadata.rs
@@ -1,4 +1,5 @@
 use serenity::{
+    builder::CreateEmbed,
     client::Context,
     framework::standard::{macros::command, CommandResult},
     futures::lock::Mutex,
@@ -17,14 +18,30 @@ pub async fn set_metadata(metadata: Metadata) {
     *mut_metadata = metadata;
 }
 
+fn create_embed<'a>(e: &'a mut CreateEmbed, m: &Metadata) -> &'a mut CreateEmbed {
+    let v: Vec<&str> = m.git.split('-').collect();
+    if v.len() == 2 {
+        e.url(format!("https://github.com/NoKrab/KraBot/commit/{}", v[1]));
+    };
+    e.title("Application Version");
+    e.field("version", m.version, true);
+    e.field("git commit", m.git, true);
+    e.field("compiled", m.date, false);
+    e
+}
+
 /// Displays the current crate version.
 /// Repository: https://github.com/NoKrab/KraBot
 #[command]
-async fn version(context: &Context, msg: &Message) -> CommandResult {
+async fn version(ctx: &Context, msg: &Message) -> CommandResult {
     let metadata = METADATA.lock().await;
+    // let me = *metadata;
     check_msg(
         msg.channel_id
-            .say(&context.http, metadata.version.to_string())
+            .send_message(&ctx.http, |m| {
+                m.embed(|e| create_embed(e, &metadata));
+                m
+            })
             .await,
     );
 
